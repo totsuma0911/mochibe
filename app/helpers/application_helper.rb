0a10f0cf56d9fa7e8e10f0554283ab32a8dfaf87
Totsuma0911 <ma19sa92no09ri11@gmail.com>
2025-12-12 22:09:38 +0900

    ヘッダーにTOPアイコンとパンくずの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  # パンくずリストの生成
+  # TOPページ以外で表示し、controller名に応じて最終階層を変更
+  def breadcrumbs
+    return nil if controller_name == 'home'
+
+    case controller_name
+    when 'chat_sessions'
+      [
+        { text: 'TOP', path: root_path },
+        { text: '分析中', path: nil }
+      ]
+    when 'analyses'
+      [
+        { text: 'TOP', path: root_path },
+        { text: '分析結果', path: nil }
+      ]
+    else
+      nil
+    end
+  end
 end
